75dbaa34c31b8c90bec0da8b3902c7815cee17dc
Aradhya-Patro <82884623+Aradhyapatro@users.noreply.github.com>
2022-09-25 20:50:13 +0530

    look into v4 upgrade

--- home/pages/_app.js
+++ home/pages/_app.js
@@ -1,10 +1,19 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import Navbar from '../components/navbar'
-import { sessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return <sessionProvider session={session}><Head><title>Next.js</title></Head><Navbar></Navbar><Component {...pageProps} /></sessionProvider>
+  return (<SessionProvider session={session} refetchInterval={5 * 60}>
+    <Head>
+      <title>
+        Next.js
+      </title>
+    </Head>
+    <Navbar>
+    </Navbar>
+    <Component {...pageProps} />
+  </SessionProvider >);
 }
 
 export default MyApp
